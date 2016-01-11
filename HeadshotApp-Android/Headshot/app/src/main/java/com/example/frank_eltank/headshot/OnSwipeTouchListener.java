d92c84f7761ac043e90b8b5dd06100c6d5a0480f
Frank <fylin134@gmail.com>
2016-01-10 19:18:45 -0600

    Flash, New Shutter Button, Cutout Swipe
    Front facing flash algorithm implemented.
    New "Invisible Bottom Shutter" implemented
    Implemented left and right swipe to change cutouts.

---
+++ HeadshotApp-Android/Headshot/app/src/main/java/com/example/frank_eltank/headshot/OnSwipeTouchListener.java
@@ -0,0 +1,66 @@
+package com.example.frank_eltank.headshot;
+
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by Frank on 1/10/2016.
+ */
+public class OnSwipeTouchListener implements View.OnTouchListener
+{
+    private final GestureDetector mGestureDetector;
+
+    public OnSwipeTouchListener (Context context){
+        mGestureDetector= new GestureDetector(context, new GestureListener());
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return mGestureDetector.onTouchEvent(motionEvent);
+    }
+
+    public void onSwipeRight() {
+
+    }
+
+    public void onSwipeLeft() {
+
+    }
+
+    private class GestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        private static final int SWIPE_THRESHOLD = 100;
+        private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            boolean result = false;
+            try {
+                float diffY = e2.getY() - e1.getY();
+                float diffX = e2.getX() - e1.getX();
+                if (Math.abs(diffX) > Math.abs(diffY)) {
+                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffX > 0) {
+                            onSwipeRight();
+                        } else {
+                            onSwipeLeft();
+                        }
+                    }
+                    result = true;
+                }
+                result = true;
+
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+            return result;
+        }
+    }
+}
